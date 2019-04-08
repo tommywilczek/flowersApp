d53ac291654ab8bfdb00e89f9fc01a8a85db7887
Tommy Wilczk <tommywilczek@utexas.edu>
2019-04-08 17:45:12 -0500

    Added Angular Material, still does not work with Karma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AllMaterialModule } from "./material-module";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AllMaterialModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
