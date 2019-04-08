2ca9ac80fe8e48f5c51a411bcbf32a393c3d1cfd
Tommy Wilczk <tommywilczek@utexas.edu>
2019-04-08 17:50:03 -0500

    Added linting to Travis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AllMaterialModule } from "./material-module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AllMaterialModule } from './material-module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
