295f424c474d7d8dbe9555690df1294f36eed590
Tommy Wilczk <tommywilczek@utexas.edu>
2019-04-09 20:51:56 -0500

    Added error handling to backend server

--- Server/app.js
+++ Server/app.js
@@ -7,7 +7,23 @@ const orderRoutes = require('./routes/orders');
 
 app.use(morgan('dev'));
 
+// Routes which should handle requests
 app.use('/flowers', flowersRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
