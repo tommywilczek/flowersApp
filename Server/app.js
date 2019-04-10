2a089b9a5bfb2f752b7e7d63402b5c6154f0d43f
Tommy Wilczk <tommywilczek@utexas.edu>
2019-04-09 20:46:48 -0500

    installed Morgan

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan'); // for logging in the console what APIs are hit
 
 const flowersRoutes = require('./routes/flowers');
 const orderRoutes = require('./routes/orders');
+
+app.use(morgan('dev'));
 
 app.use('/flowers', flowersRoutes);
 app.use('/orders', orderRoutes);
