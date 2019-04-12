8f2c12e39d2e2f2557a9ef21302ea08ffd9122d8
Tommy Wilczk <tommywilczek@utexas.edu>
2019-04-12 00:47:28 -0500

    Can extract key/value pairs from JSON

--- src/app/show-flowers/show-flowers.component.ts
+++ src/app/show-flowers/show-flowers.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { ShowFlowersService } from './show-flowers.service';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-show-flowers',
   templateUrl: './show-flowers.component.html',
-  styleUrls: ['./show-flowers.component.css']
+  styleUrls: ['./show-flowers.component.css'],
+  providers: [ ShowFlowersService ]
 })
 export class ShowFlowersComponent implements OnInit {
 
-  constructor() { }
+  public flowers: any[];
+
+  constructor(private showFlowersService: ShowFlowersService) { }
 
   ngOnInit() {
+    this.showAllFlowers();
+  }
+
+  showAllFlowers() {
+    this.showFlowersService.getAllFlowers().subscribe(res => {
+      this.flowers = res;
+      console.log('this.flowers = ', this.flowers);
+    })
+  }
+
+  deleteFlower(flower) {
+    this.showFlowersService.deleteFlower(flower).subscribe(res => {
+      console.log('deleted:', flower);
+      this.showAllFlowers();
+    });
+  }
+
+  extractJsonKeys(flowers) {
+    console.log(flowers);
+
+    Object.keys(flowers).forEach(function(index) {
+      Object.keys(flowers[index]).forEach(function(fieldName) {
+        if (fieldName === 'extraFields') {
+          Object.keys(flowers[index][fieldName]).forEach(function(extraFieldName) {
+            console.table('Key : ' + extraFieldName + ', Value : ' + flowers[index][fieldName][extraFieldName]);
+          });
+        } else {
+          console.table('Key : ' + fieldName + ', Value : ' + flowers[index][fieldName]);
+        }
+      });
+    });
+
+    // for (var index in flowers) {
+    //   if (flowers.hasOwnProperty(index)) {
+    //     console.log(flowers[index]);
+    //     for(var jsonKey in flowers[index]) {
+    //       if (jsonKey === 'extraFields') {
+    //         for (var extraFieldsKey in flowers[jsonKey]) {
+    //           console.log('extraFieldsKey: ', flowers[jsonKey][extraFieldsKey]);
+    //         }
+    //         console.log(flowers[jsonKey]);
+    //       }
+    //       console.log(jsonKey);
+    //     }
+    //   }
+
+    // }
   }
 
 }
--- src/app/show-flowers/show-flowers.service.ts
+++ src/app/show-flowers/show-flowers.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowFlowersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllFlowers() {
+    return this.http.get('/api/flowers');
+    }
+  
+  deleteFlower(flower) {
+    return this.http.delete('/api/flowers/' + flower._id);
+  }
+
 }
